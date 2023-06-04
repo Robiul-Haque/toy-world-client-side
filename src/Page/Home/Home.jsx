7b239ed01479142d0e8ba9b9f035d6c5dfe569f8
Robiul-Haque <robiulcoc420@gmail.com>
2023-06-04 12:57:12 +0600

    complete image gallery

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,11 +1,12 @@
 // import React from 'react';
 import Carousel from "./Component/Carousel/Carousel";
-// import ImageGallery from "./Component/ImageGallery/ImageGallery";
+import ImageGallery from "./Component/ImageGallery/ImageGallery";
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
+            <ImageGallery></ImageGallery>
         </div>
     );
 };
